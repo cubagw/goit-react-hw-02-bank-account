002d5f019821629c76c591242f09725d0d6707e4
dmytro vitenko <cubagw84@gmail.com>
2019-11-05 08:05:42 +0200

    Updates

---
+++ precache-manifest.e4ab1b90e542ff2aa6e333926039f0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d71250c2d4b1167a515bd2ffc1162e",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "fdfd2cdc007fa6bab7a9",
+    "url": "/goit-react-hw-02-reader/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "59e9be953238a608556b",
+    "url": "/goit-react-hw-02-reader/static/css/main.8f90787f.chunk.css"
+  },
+  {
+    "revision": "fdfd2cdc007fa6bab7a9",
+    "url": "/goit-react-hw-02-reader/static/js/2.ea5c5921.chunk.js"
+  },
+  {
+    "revision": "59e9be953238a608556b",
+    "url": "/goit-react-hw-02-reader/static/js/main.5064bc9a.chunk.js"
+  },
+  {
+    "revision": "1d7639d31312f9777566",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.f3397c26.js"
+  }
+]);
